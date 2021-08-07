2d3f7961fcf5c456645c3aba96866c6faabb786f
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-07 14:22:41 -0300

    exercicio03

--- exe03/main.c
+++ exe03/main.c
@@ -5,8 +5,14 @@ int main() {
    scanf("%i", &n);
    if(n==0){
       printf("NUMERO NEUTRO\n");
-   } else if(n<){
-
+   } else if(n<0 && n%2==0){
+      printf("NUMERO PAR NEGATIVO");
+   } else if(n<0 && n%2!=0){
+      printf("NUMERO IMPAR NEGATIVO");
+   } else if(n>0 && n%2==0){
+      printf("NUMERO PAR POSITIVO");
+   } else if(n>0 && n%2!=0){
+      printf("NUMERO IMPAR POSITIVO");
    }
    return 0;
 }
