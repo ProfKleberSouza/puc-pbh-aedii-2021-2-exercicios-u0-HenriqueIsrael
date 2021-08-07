a753da6e34e7b46629b333b6b13ed8a6e6081c42
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-07 20:00:37 -0300

    exercicio06

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int x, y, z;
+
+   scanf("%i %i %i", &x, &y, &z);
+
+   if((x > y+z) || (y > z+x) || (z > y+x)){
+      printf("OS LADOS NAO FORMAM UM TRIANGULO");
+   } else if(x==y && y==z){
+      printf("TRIANGULO EQUILATERO");
+   } else if(x==y || y==z || x==z){
+         printf("TRIANGULO ISOSCELES");
+   } else if(x!=y && y!=z){
+         printf("TRIANGULO ESCALENO");
+   }
    return 0;
 }
