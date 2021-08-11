9578b6136f1c7587d84d41481d14d2abde3e4b22
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-10 21:37:05 -0300

    exercicio03

--- exe03/main.c
+++ exe03/main.c
@@ -2,9 +2,11 @@
 
 int main() {
    int n;
-
+   //Recebe a entrada.
    scanf("%i", &n);
 
+   /*Condição para verificar se o número é
+    neutro, par, impar, negativo ou positivo, podendo atender mais de uma dessas condições.*/
    if(n==0){
       printf("NUMERO NEUTRO\n");
    } else if(n<0 && n%2==0){
