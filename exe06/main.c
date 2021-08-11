10490d72fff7cf222b6a301f7d44990465afe83a
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-10 22:02:11 -0300

    exercicio06

--- exe06/main.c
+++ exe06/main.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
 int main() {
+   //Declaração de variáveis.
    int x, y, z;
 
+   //Recebe os lados do triângulo.
    scanf("%i %i %i", &x, &y, &z);
 
+   //Condição para verificar se os lados formam um triângulo.
    if((x > y+z) || (y > z+x) || (z > y+x)){
       printf("OS LADOS NAO FORMAM UM TRIANGULO");
-   } else if(x==y && y==z){
+   }
+   //Se os lados forem iguais, triângulo equilátero.
+    else if(x==y && y==z){
       printf("TRIANGULO EQUILATERO");
-   } else if(x==y || y==z || x==z){
+   }
+   //Se pelo menos dois lados forem iguais, triângulo isosceles.
+    else if(x==y || y==z || x==z){
          printf("TRIANGULO ISOSCELES");
-   } else if(x!=y && y!=z){
+   } 
+   //Se todos os lados forem diferentes, triângulo escaleno.
+   else if(x!=y && y!=z){
          printf("TRIANGULO ESCALENO");
    }
    
