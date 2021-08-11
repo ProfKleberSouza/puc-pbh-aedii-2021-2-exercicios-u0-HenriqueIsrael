77b078430647a795e392fe225ff6beeabc5a6c40
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-10 21:49:28 -0300

    exercicio05

--- exe05/main.c
+++ exe05/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
+   //Declaração de variáveis.
    int n1, n2, n3, n4;
    float media=0;
-
+   //Recebe os valores das notas.
    scanf("%i %i %i %i", &n1, &n2, &n3, &n4);
-
+   //Calcula a média.
    media = (float)(n1+n2+n3+n4)/4;
-
+   //Condição para verificar se o aluno está aprovado.
    if(media>=6){
       printf("NOTA = %0.1f (APROVADO)\n", media);
    } else{
