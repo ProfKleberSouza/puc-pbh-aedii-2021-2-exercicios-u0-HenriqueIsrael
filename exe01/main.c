6fcdf937435b2e4027f2aa4e18ee9a2bf8c9ee14
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-10 21:17:37 -0300

    exercicio01

--- exe01/main.c
+++ exe01/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
+   //Declaração de variáveis.
    int n1, n2;
 
+   //Recebe os dois números.
    scanf("%i %i", &n1, &n2);
 
+   //Mostra o somatório dos números.
    printf("SOMA = %i\n", n1+n2);
    
    return 0;
--- exe09/main.c
+++ exe09/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int i,n;
+   float media=0,somaNotas=0;
+   
+   scanf("%i", &n);
+
+   char nome[50];
+   int nota[n];
+
+   for(i=0;i<n;i++){
+      scanf("%s %i",&nome[i],&nota[i]);
+      somaNotas += nota[i];
+   }
+
+   media = somaNotas/n;
+
+   printf("NOTA MEDIA = %.1f",media);
+
+   for(i=0;i<3;i++)
+
    return 0;
 }
