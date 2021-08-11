7465788e15427cbb5310765d995d2cd028964456
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-10 22:34:50 -0300

    exercicio10

--- exe10/main.c
+++ exe10/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main() {
+   //Declaração de variáveis.
    int i,vet[10];
    
+   //Laço de repetição que percorre o vetor.
    for(i=0;i<10;i++){
       scanf("%i",&vet[i]);
    }
 
+   //Printa os valores do vetor na posição i.
    for(i=9;i>=0;i--){
       printf("%i\n",vet[i]);
    }
