cf00a3d0e347eddaa713b1bf8a556867706d094d
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-10 21:31:54 -0300

    exercicio02

--- exe02/main.c
+++ exe02/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
+   //Declaração de variáveis.
    int R,V,T,D,L;
-
+   //Recebe os valores T, V e R.
    scanf("%i %i %i", &T, &V, &R);
-
+   // Distância percorrida = tempo * velocidade.
    D = T * V;
-
+   //Litros = distância / rendimento médio.
    L = D/R;
-
+   //Printa os valores da tela.
    printf("R = %i\n",R);
    printf("V = %i\n",V);
    printf("T = %i\n",T);
