c22e31c3e0f8cda2095a045e7e07dbdfb81fdd54
HenriqueIsrael <henriqueisraeloliveira74@gmail.com>
2021-08-10 22:24:59 -0300

    exercicio08

--- exe08/main.c
+++ exe08/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int main() {
+   //Declaração de variável.
    int n;
+   //Laço de repetição a ser executado enquanto n for diferente de zero.
    do{
+      //Recebe valores para n.
       scanf("%i",&n);
+      //Condição para verificar se n é negativo.
       if(n<0){
          printf("NEGATIVO\n");
       }  
+      //Condição para verificar se n é positivo.
          else if(n>0){
             printf("POSITIVO\n");
          }     
